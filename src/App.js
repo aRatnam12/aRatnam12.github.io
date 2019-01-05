8d81812f2973c68915160b5905163ff2dd6ce92c
Ansh Ratnam <ansh@uber.com>
2019-01-05 13:26:12 -0800

    Initial webpage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
+import React from "react";
+import navMap, { type NavItem } from "./nav-map";
+import SideNav from "./side-nav";
+import MainPanel from "./main-panel";
+import "./App.css";
 
-class App extends Component {
+type State = {
+  activeNavItem: NavItem
+};
+
+class App extends React.Component<*, State> {
+  state = {
+    activeNavItem: Object.keys(navMap)[0]
+  };
+
+  handleUpdateActiveNavItem = (navItem: NavItem): void => {
+    this.setState({
+      activeNavItem: navItem
+    });
+  };
+
   render() {
+    const { activeNavItem } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 className="App__welcome">Welcome to Ansh's webpage!</h1>
+        <div className="App__main">
+          <SideNav
+            activeNavItem={activeNavItem}
+            onUpdateActiveNavItem={this.handleUpdateActiveNavItem}
+          />
+          <MainPanel activeNavItem={activeNavItem} />
+        </div>
       </div>
     );
   }
